6bab7355bcecf259821c1a748abb914209bb4094
Balaji Setti <balaji.setti@diconium.com>
2020-05-20 20:17:16 +0530

    DCC-259 Saved card update

--- DrPay/Controller/Creditcard/Savedrsource.php
+++ DrPay/Controller/Creditcard/Savedrsource.php
@@ -24,11 +24,15 @@ class Savedrsource extends \Magento\Framework\App\Action\Action
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Magento\Checkout\Model\Session $checkoutSession,
-        \Digitalriver\DrPay\Logger\Logger $logger,
-        \Digitalriver\DrPay\Helper\Data $helper
+		\Magento\Directory\Model\Region $regionModel,
+		\Magento\Customer\Api\AddressRepositoryInterface $addressRepository,
+        \Digitalriver\DrPay\Helper\Data $helper,
+        \Digitalriver\DrPay\Logger\Logger $logger
     ) {
         $this->helper =  $helper;
         $this->_checkoutSession = $checkoutSession;
+		$this->regionModel      = $regionModel;
+		$this->addressRepository = $addressRepository;
         $this->_logger = $logger;
         parent::__construct($context);
     }
@@ -50,6 +54,52 @@ class Savedrsource extends \Magento\Framework\App\Action\Action
         
         $source_id = $this->getRequest()->getParam('source_id');
         $quote = $this->_checkoutSession->getQuote();
+		if ($optionId = $this->getRequest()->getParam('option_id')) {
+			$cardResult = $this->helper->getSavedCards();
+			foreach($cardResult['paymentOptions']['paymentOption'] as $id => $card) {
+				if($optionId == $id){
+					$region = $this->regionModel->loadByCode($card['address']['state'], $card['address']['country'])->getData();
+					$street = $card['address']['line1'];
+					$street .= (!empty($card['address']['line2'])) ? (' '.$card['address']['line2']) : null;
+					$street .= (!empty($card['address']['line3'])) ? (' '.$card['address']['line3']) : null;
+
+					$street = trim($street);
+					$phone = $quote->getBillingAddress()->getTelephone();
+					$billingAddress = [
+						'firstname'     => (!empty($card['owner']['firstName'])) ? trim($card['owner']['firstName']) : null,
+						'lastname'      => (!empty($card['owner']['lastName'])) ? trim($card['owner']['lastName']) : null,
+						'street'        => $street,
+						'city'          => $card['address']['city'],
+						'postcode'      => $card['address']['postalCode'],
+						'country_id'    => $card['address']['country'],
+						'region'        => !empty($region['name']) ? $region['name'] : null,
+						'region_id'     => !empty($region['region_id']) ? $region['region_id'] : null,
+						'telephone'     => $phone
+					];
+					$quote->getBillingAddress()->addData($billingAddress);
+					$quote->save();
+					break;
+				}
+			}
+		}else{
+			$customerAddressId = $quote->getBillingAddress()->getCustomerAddressId();
+			if($customerAddressId){
+				$address = $this->addressRepository->getById($customerAddressId);
+				$billingAddress = [
+                    'firstname'     => $address->getFirstname(),
+                    'lastname'      => $address->getLastname(),
+                    'street'        => $address->getStreet(),
+                    'city'          => $address->getCity(),
+                    'postcode'      => $address->getPostcode(),
+                    'country_id'    => $address->getCountryId(),
+                    'region'        => $address->getRegion()->getRegion(),
+                    'region_id'     => $address->getRegionId(),
+                    'telephone'     => $address->getTelephone()
+                ];
+				$quote->getBillingAddress()->addData($billingAddress);
+				$quote->save();
+			}
+		}
         $cartResult = $this->helper->createFullCartInDr($quote, 1);
         if ($cartResult) {
             if ($this->getRequest()->getParam('source_id')) {
