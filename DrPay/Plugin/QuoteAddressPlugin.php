8b9406dc10b2e8d624a33f25e5893a7c3550b065
Balaji Setti <balaji.setti@diconium.com>
2020-06-02 16:34:27 +0530

    DCC-273 mini cart not updating the subtotal as a logged in user

--- DrPay/Plugin/QuoteAddressPlugin.php
+++ DrPay/Plugin/QuoteAddressPlugin.php
@@ -54,26 +54,26 @@ class QuoteAddressPlugin
     ) {
         $enableDrPayValue = $this->getDrPayEnable();
         if ($enableDrPayValue) {
-                        $quote = $subject->getQuote();
-                        $address =  $quote->getBillingAddress();
-                        if($address->getCustomerId() && $address->getCity() == null) {
-                           $customer = $this->_customerFactory->create()->load($address->getCustomerId());
-                           $billingAddressId = $customer->getDefaultBilling();
-                           if($billingAddressId) {
-                                $billingAddress = $this->_addressFactory->create()->load($billingAddressId);
-                                $address->setFirstName($billingAddress->getFirstname());
-                                $address->setLastName($billingAddress->getLastname());
-                                $address->setMiddlename($billingAddress->getMiddlename());
-                                $address->setCompany($billingAddress->getCompany());
-                                $address->setTelephone($billingAddress->getTelephone());
-                                $address->setStreet($billingAddress->getStreet());
-                                $address->setCity($billingAddress->getCity());
-                                $address->setPostCode($billingAddress->getPostCode());
-                                $address->setRegionId($billingAddress->getRegionId());
-                                $address->setRegion($billingAddress->getRegion());
-                                $address->setCountryId($billingAddress->getCountryId());
-                           }
-                        }
+			$quote = $subject->getQuote();
+			$address =  $quote->getBillingAddress();
+			if($address->getCustomerId() && $address->getCity() == null) {
+			   $customer = $this->_customerFactory->create()->load($address->getCustomerId());
+			   $billingAddressId = $customer->getDefaultBilling();
+			   if($billingAddressId) {
+					$billingAddress = $this->_addressFactory->create()->load($billingAddressId);
+					$address->setFirstName($billingAddress->getFirstname());
+					$address->setLastName($billingAddress->getLastname());
+					$address->setMiddlename($billingAddress->getMiddlename());
+					$address->setCompany($billingAddress->getCompany());
+					$address->setTelephone($billingAddress->getTelephone());
+					$address->setStreet($billingAddress->getStreet());
+					$address->setCity($billingAddress->getCity());
+					$address->setPostCode($billingAddress->getPostCode());
+					$address->setRegionId($billingAddress->getRegionId());
+					$address->setRegion($billingAddress->getRegion());
+					$address->setCountryId($billingAddress->getCountryId());
+			   }
+			}
             //if (!$quote->isVirtual()) {
                 //Create the cart in DR
                 $this->drHelper->createFullCartInDr($quote);
