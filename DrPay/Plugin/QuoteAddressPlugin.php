5cdee1faff93892033b3ab8fb7bd27149b434f7b
Balaji Setti <balaji.setti@diconium.com>
2020-06-02 15:07:10 +0530

    [BUGFIX] DCC-273 - Minicart update with address code in createFullCartInDr function.

--- DrPay/Plugin/QuoteAddressPlugin.php
+++ DrPay/Plugin/QuoteAddressPlugin.php
@@ -14,14 +14,22 @@ class QuoteAddressPlugin
     
     protected $scopeConfig;
     
+    protected $_customerFactory;
+    
+    protected $_addressFactory;
+    
     const XML_PATH_ENABLE_DRPAY = 'dr_settings/config/active';
     
     public function __construct(
         \Digitalriver\DrPay\Helper\Data $drHelper,
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Customer\Model\CustomerFactory $customerFactory,
+        \Magento\Customer\Model\AddressFactory $addressFactory
     ) {
          $this->drHelper= $drHelper;
          $this->scopeConfig = $scopeConfig;
+         $this->_customerFactory = $customerFactory;
+         $this->_addressFactory = $addressFactory;
     }
     /**
      * Get DrPay Module Status
@@ -46,7 +54,26 @@ class QuoteAddressPlugin
     ) {
         $enableDrPayValue = $this->getDrPayEnable();
         if ($enableDrPayValue) {
-			$quote = $subject->getQuote();
+                        $quote = $subject->getQuote();
+                        $address =  $quote->getBillingAddress();
+                        if($address->getCustomerId() && $address->getCity() == null) {
+                           $customer = $this->_customerFactory->create()->load($address->getCustomerId());
+                           $billingAddressId = $customer->getDefaultBilling();
+                           if($billingAddressId) {
+                                $billingAddress = $this->_addressFactory->create()->load($billingAddressId);
+                                $address->setFirstName($billingAddress->getFirstname());
+                                $address->setLastName($billingAddress->getLastname());
+                                $address->setMiddlename($billingAddress->getMiddlename());
+                                $address->setCompany($billingAddress->getCompany());
+                                $address->setTelephone($billingAddress->getTelephone());
+                                $address->setStreet($billingAddress->getStreet());
+                                $address->setCity($billingAddress->getCity());
+                                $address->setPostCode($billingAddress->getPostCode());
+                                $address->setRegionId($billingAddress->getRegionId());
+                                $address->setRegion($billingAddress->getRegion());
+                                $address->setCountryId($billingAddress->getCountryId());
+                           }
+                        }
             //if (!$quote->isVirtual()) {
                 //Create the cart in DR
                 $this->drHelper->createFullCartInDr($quote);
