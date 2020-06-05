3a56b67bb1d74b09c4c2bf71fbd035889a8aaf42
Balaji S <balaji.s@embitel.com>
2020-06-05 11:21:53 +0530

    DCC-217 Duplicated addresses after credit card payment failure

--- DrPay/Observer/CreateDrOrder.php
+++ DrPay/Observer/CreateDrOrder.php
@@ -19,18 +19,19 @@ use Magento\Framework\Exception\CouldNotSaveException;
  */
 class CreateDrOrder implements ObserverInterface
 {
-        /**
-         * @param \Digitalriver\DrPay\Helper\Data            $helper
-         * @param \Magento\Checkout\Model\Session            $session
-         * @param \Magento\Store\Model\StoreManagerInterface $storeManager
-         */
+	/**
+	 * @param \Digitalriver\DrPay\Helper\Data            $helper
+	 * @param \Magento\Checkout\Model\Session            $session
+	 * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+	 */
     public function __construct(
         \Digitalriver\DrPay\Helper\Data $helper,
         \Magento\Checkout\Model\Session $session,
 		\Digitalriver\DrPay\Model\DrConnector $drconnector,
 		\Magento\Framework\Json\Helper\Data $jsonHelper,
 		\Magento\Framework\Event\ManagerInterface $eventManager,
-        \Magento\Store\Model\StoreManagerInterface $storeManager
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Customer\Api\AddressRepositoryInterface $addressRepository
     ) {
         $this->helper =  $helper;
         $this->session = $session;
@@ -38,6 +39,7 @@ class CreateDrOrder implements ObserverInterface
 		$this->jsonHelper = $jsonHelper;
         $this->_storeManager = $storeManager;
 		$this->_eventManager = $eventManager;
+                $this->addressRepository = $addressRepository;
     }
 
     /**
@@ -54,7 +56,15 @@ class CreateDrOrder implements ObserverInterface
         if ($quote->getPayment()->getMethod() == \Digitalriver\DrPay\Model\CreditCard::PAYMENT_METHOD_CREDITCARD_CODE || $quote->getPayment()->getMethod() == \Digitalriver\DrPay\Model\ApplePay::PAYMENT_METHOD_APPLE_PAY_CODE) {
             $result = $this->helper->createFullCartInDr($quote, 1);
             $accessToken = $this->session->getDrAccessToken();
+            $addressId = $quote->getShippingAddress() ? $quote->getShippingAddress()->getCustomerAddressId(): null;
+            $billingAddressId = $quote->getBillingAddress() ? $quote->getBillingAddress()->getCustomerAddressId(): null;
             if ($this->session->getDrQuoteError()) {
+                if ($addressId && $quote->getShippingAddress()->getSaveInAddressBook()) {
+                    $this->addressRepository->deleteById($addressId);
+                }
+                if ($billingAddressId && $quote->getBillingAddress()->getSaveInAddressBook()) {
+                    $this->addressRepository->deleteById($billingAddressId);
+                }
                 throw new CouldNotSaveException(__('Unable to Place Order'));
             } else {
                 $totals = $result["cart"]["pricing"]["orderTotal"];
@@ -66,6 +76,12 @@ class CreateDrOrder implements ObserverInterface
 				$cartresult = $this->helper->getDrCart();
                 $result = $this->helper->createOrderInDr($accessToken);
                 if ($result && isset($result["errors"])) {
+                    if ($addressId && $quote->getShippingAddress()->getSaveInAddressBook()) {
+                        $this->addressRepository->deleteById($addressId);
+                    }
+                    if ($billingAddressId && $quote->getBillingAddress()->getSaveInAddressBook()) {
+                        $this->addressRepository->deleteById($billingAddressId);
+                    }
                     throw new CouldNotSaveException(__('Unable to Place Order'));
                 } else {
 					$this->_eventManager->dispatch('dr_place_order_success', ['order' => $order, 'quote' => $quote, 'result' => $result, 'cart_result' => $cartresult]);
